454fae445cc99f14cba1266e0728589ba25e9b0a
abdulrahmanbhatti <abdulrehmanbhatti0010@gmail.com>
2026-05-24 07:43:30 +0500

    js

---
+++ code.js
@@ -0,0 +1,137 @@
+// let name = "Rahman";
+// console.log(name);
+// const country = "PAKISTAN";
+// console.log(country);
+// let age = 21 ; 
+// console.log(age);
+// let isStudent = true ;
+// console.log(isStudent);
+
+// let a = 10 ; 
+// let b = 20 ;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a==b);
+
+
+// let ages = 20 ;
+// let id = true ;
+// console.log(age>=18 && id );
+
+
+// let age = 18 ;
+//  if(age>=15){
+//     console.log("abdul Rahman ");
+//  }else {
+//     console.log("bhatti");
+//  }
+
+// let marks = 85 ;
+//  if ( marks > 90 ){
+//     console.log ("A");
+//  }
+//    else if (marks >=86){
+//     console.log("B");
+//    }
+//    else{
+//     console.log("pass");
+//    }
+
+// let day = 2 ;
+// switch(day){
+//     case 1 :
+//         console.log("monday");
+//         break;
+//    case 2 :
+//         console.log("Tuesday ");
+//         break;
+//    case 3 :
+//         console.log("wednesday");
+//         break;
+// }
+
+// for ( let i = 0 ; i<100 ; i++){
+//     console.log(i);
+// }
+
+
+// let i = 0 ;
+//  while ( i <=10 ){
+//     console.log(i);
+//     i ++;
+//  }
+
+// let i = 0 ;
+//  do {
+//     console.log(i);
+//     i ++;
+//  }while(i<10);
+
+
+
+// function 
+
+// function print(){
+//     console.log("hello");
+// }
+// print();
+
+// function add (a , b ){
+//     console.log(a+b);
+// }
+// add ( 3,5 );
+
+
+// function squre (a){
+//     return a*a;
+// }
+
+// console.log(squre(8));
+
+
+
+// const multiply = (a,b) =>{
+//     return a *b;
+// }
+// console.log(multiply(4,5));
+
+
+// let fruits = ["apple","mango","banana"];
+// console.log(fruits);
+// console.log(fruits[0]);
+// fruits.pop();
+// console.log(fruits);
+
+// for ( let i =0 ; i < fruits.length ;i ++){
+//     console.log(fruits[i]);
+// }
+
+// let student = {
+//     name : " ali", age : 20 , city : "karachi"
+
+// };
+
+// console.log(student.name);
+
+// let car = {
+//     brand : "honda",
+
+//     start: function (){
+//         console.log("car started");
+//     }
+// };
+// car.start();
+
+
+function changeText(){
+    document.getElementById("title").innerHTML= "WELLCOME";
+
+}
+
+
