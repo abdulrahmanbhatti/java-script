d7ec9a3721ef7f0f8209f666c26a0e70d58cf150
abdulrahmanbhatti <abdulrehmanbhatti0010@gmail.com>
2026-06-18 09:49:22 +0500

    class

---
+++ DOM/Rendem practice/oops/js.js
@@ -0,0 +1,28 @@
+// const  employee = {
+//     calText(){
+//         console.log("tex rate is 10 ")
+//     },
+
+// }
+
+
+// const AbdulRahman ={
+//     salary : 5000,
+//       calText(){
+//         console.log("tex rate is 10 ")
+//     },
+
+// }
+
+
+// AbdulRahman.__proto__=employee;
+
+// // if object has same name of prototype this object method will be used
+
+
+// class & object
+
+class car {
+    
+}
+// class & object
