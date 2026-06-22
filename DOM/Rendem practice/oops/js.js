35b8d90aeab7b98dbd062cd08ddc921a9347c085
abdulrahmanbhatti <abdulrehmanbhatti0010@gmail.com>
2026-06-22 21:29:43 +0500

    js advance concept

--- DOM/Rendem practice/oops/js.js
+++ DOM/Rendem practice/oops/js.js
@@ -22,33 +22,218 @@
 
 // class & object
 
-class student {
-    calavg(){
-        let eng=Number(prompt("enter your english marks "));
-        let com = Number(prompt("enter your computer marks "));
-        let math =Number(prompt("enter your math marks "));
-        let avg = (eng+com+math)/3;
-        console.log(`your marks avg is ${avg}`);
-    }
+// class student {
+    
 
-    semester(){
-        let depart = "IS";
-        let semesters="five";
-        console.log(`${depart} and ${semesters}`)
+//     constructor (name , roll){
+//         console.log("this is a parameterize constructor ")
+//        this.name = name;
+//        this.roll=roll;
+//        console.log(name )
+//        console.log(roll)
+//     }
 
-    }
 
-}
+//     calavg(){
+//         let eng=Number(prompt("enter your english marks "));
+//         let com = Number(prompt("enter your computer marks "));
+//         let math =Number(prompt("enter your math marks "));
+//         let avg = (eng+com+math)/3;
+//         console.log(`your marks avg is ${avg}`);
+//     }
 
-let st1 = new student();
-st1.calavg();
+//     semester(){
+//         let depart = "IS";
+//         let semesters="five";
+//         console.log(`${depart} and ${semesters}`)
 
-let st2= new student();
-st2.semester();
+//     }
 
+// }
+
+
+// let st1 = new student("abdul rahman" , "salman");
+// st1.calavg();
+
+// console.log(st1);
+
+
+
+// let st2= new student();
+// st2.semester();
+
+
+
+// class parson {
+//     eat(){
+//         console.log("this is a eat function ")
+//     }
+
+
+//     sleep(){
+//         console.log("this is a sleep function")
+//     }
+// }
+
+// class student extends parson{
+//     work(){
+//         console.log('this is a work function')
+//     }
+// }
+
+// class teacher extends student{
+//     salary(){
+//         console.log("this is a salary function")
+
+// }
+// }
+
+
+
+// let p1=  new parson;
+// p1.sleep();
+
+
+// let st1 = new student;
+
+// st1.eat();
+// st1.work();
+
+
+// let tech1 = new teacher;
+// tech1.eat();
+// tech1.work();
+// tech1.salary();
+
+
+
+// let data  ="this is a data "
+
+// class users{
+//    constructor(name , email){
+//     this.name= name ;
+//     this.email=email;
+//     // console.log(name);
+//     // console.log(email)
+
+//    }
+
+//    showdata(){
+//     console.log("this is a web data !");
+//     console.log(data)
+//    }
+// }
+
+
+// class admin{
+    
+// }
+
+// let user1=new users("ali","ahmad");
+// console.log(user1)
+
+// user1.showdata();
 
 
 
 
 
 // class & object
+
+
+// console.log ("this is one question ")
+// console.log ("this is two question ")
+// console.log ("this is three question ")
+
+
+// setTimeout(( )=>{
+//    console.log("this program will be lode after time ")
+
+
+// },3000);
+
+
+//    console.log("this program will be lode 3s time ")
+
+
+
+// function user ( name , age ){
+//    console.log(`your name is ${name}
+//        and age is ${age}`)
+   
+// }
+
+
+// function setfun(name,age,Callback){
+
+
+//    // let name="abdul";
+//    // let age = 45;
+//    Callback(name,age);
+
+// }
+
+// setfun("HGJH",45,user);
+
+function sum (a,b){
+   console.log(a+b)
+}
+
+function addSum (sum){
+   // a=Number(prompt("Enter Your First Number "))
+   // b=Number(prompt("Enter Your Second Number "))
+   // sum(a,b)
+   sum(12,32); 
+
+}
+
+addSum(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function user ( name  ){
+//    console.log(`your name is ${name}`)
+   
+// }
+
+
+// function setfun(callback){
+
+
+//    let name="abdul";
+
+//   callback(name);
+
+// }
+
+// setfun(user);
+
+
+
+
+
+
+
+
